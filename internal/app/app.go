9b4f2b3b5eae9ecaabe48fba2dcc39d10c20d9c0
ChethiyaNishanath <chethiyanishanath@gmail.com>
2025-11-09 12:27:59 +0530

    Centralized App struct

---
+++ internal/app/app.go
@@ -0,0 +1,48 @@
+package app
+
+import (
+	"database/sql"
+	"user-management/internal/db/sqlc"
+	"user-management/internal/user"
+	"user-management/internal/validation"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator/v10"
+)
+
+type App struct {
+	DB        *sql.DB
+	Validator *validator.Validate
+	Queries   *sqlc.Queries
+
+	UserHandler *user.Handler
+}
+
+func NewApp(db *sql.DB) *App {
+	validate := validator.New()
+	validation.RegisterValidations(validate)
+
+	queries := sqlc.New(db)
+
+	userRepo := user.NewRepository(queries)
+	userService := user.NewService(userRepo)
+	userHandler := user.NewHandler(userService, validate)
+
+	return &App{
+		DB:          db,
+		Validator:   validate,
+		Queries:     queries,
+		UserHandler: userHandler,
+	}
+}
+
+func (a *App) RegisterRoutes(r chi.Router) {
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", a.UserHandler.CreateUser)
+		r.Get("/", a.UserHandler.GetUsers)
+		r.Get("/{id}", a.UserHandler.GetUserById)
+		r.Patch("/{id}", a.UserHandler.UpdateUserById)
+		r.Delete("/{id}", a.UserHandler.DeleteUserById)
+	})
+}

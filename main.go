716e82ae34a2da00ebba18f26895bf812289e80c
ChethiyaNishanath <chethiyanishanath@gmail.com>
2025-11-09 12:30:40 +0530

    Configure database, routes, server, log levels, graceful shutdown

--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"user-management/internal/app"
+	"user-management/internal/db"
+
+	"github.com/go-chi/chi/v5"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	slog.SetLogLoggerLevel(slog.LevelInfo)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	dbConn := db.Connect()
+	defer dbConn.Close()
+
+	newApp := app.NewApp(dbConn)
+	r := chi.NewRouter()
+	newApp.RegisterRoutes(r)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
 	}
+
+	go func() {
+		slog.Info("Server running on http://localhost:8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			slog.Error("Server failed", "error", err)
+		}
+	}()
+
+	<-ctx.Done()
+	slog.Info("Shutting down gracefully...")
+
+	stop()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("Forced server shutdown", "error", err)
+	}
+
+	if err := dbConn.Close(); err != nil {
+		slog.Error("Error closing DB", "error", err)
+	}
+
+	slog.Info("Shutdown complete")
 }

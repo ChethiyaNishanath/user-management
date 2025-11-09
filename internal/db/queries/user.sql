f18c019e0feba6395e24fe185c25ce0f9b586469
ChethiyaNishanath <chethiyanishanath@gmail.com>
2025-11-09 12:26:09 +0530

    User Schema and query generation using sqlc

---
+++ internal/db/queries/user.sql
@@ -0,0 +1,25 @@
+-- name: CreateUser :one
+INSERT INTO USERS (USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE, AGE, STATUS)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+RETURNING *;
+
+-- name: FindUserById :one
+SELECT * FROM USERS WHERE USER_ID = $1 LIMIT 1;
+
+-- name: ListAllUsers :many
+SELECT * FROM USERS;
+
+-- name: DeleteUserByID :exec
+DELETE FROM USERS WHERE USER_ID = $1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET
+    FIRST_NAME = COALESCE(sqlc.narg('first_name'), FIRST_NAME),
+    LAST_NAME  = COALESCE(sqlc.narg('last_name'), LAST_NAME),
+    EMAIL      = COALESCE(sqlc.narg('email'), EMAIL),
+    PHONE      = COALESCE(sqlc.narg('phone'), PHONE),
+    AGE        = COALESCE(sqlc.narg('age'), AGE),
+    STATUS     = COALESCE(sqlc.narg('status'), STATUS)
+WHERE user_id = sqlc.arg('user_id')
+RETURNING *;
